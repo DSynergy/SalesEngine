9b67dc20e7a8f3b4f5654f32141d56682052de15
Edward McCarthy <efmccarthy26@gmail.com>
2014-11-25 10:40:47 -0700

    Finished generic tests (merchant,invoice et al)

--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -1,6 +1,16 @@
 require_relative 'test_helper'
 
 class InvoiceTest < Minitest::test
+  def test_has_attributes
+    data = {id: "494", customer_id: "949", merchant_id: "8989898",
+      created_at: "2014-11-20 18:00:00 UTC", updated_at: "2013-09-18 12:00:08 UTC"
+    }
 
+    invoice = Invoice.new(data, slef)
+
+    assert_equal 494, invoice.id
+    assert_equal 949, invocie.customer_id
+    assert_equal 8989898, invoice.merchant_id
+  end
 
 end
--- test/items_test.rb
+++ test/items_test.rb
@@ -1,6 +1,20 @@
 require_relative 'test_helper'
 
 class ItemsTest < Minitest::test
+    def test_has_attributes
+      data = {id: 540, name: "box", description: "cardboard",
+      unit_price: "80000", merchant_id: 1,
+      created_at: "11:45", updated_at: "10:07"}
 
+      item = Item.new(data, self)
+
+      assert_equal 540, item.id
+      assert_equal "box", item.name
+      assert_equal "cardboard", item.description
+      assert_equal ("80000").to_i/100, item.unit_price.to_i
+      assert_equal 1, item.merchant_id
+      assert_equal "11:45", item.created_at
+      assert_equal "10:07", item.updated_at
+  end
 
 end
