6fe82f935a9b6d1d3f24956b7515ec0e48cb32f5
Edward McCarthy <efmccarthy26@gmail.com>
2014-12-02 11:56:21 -0700

    tried moving MetaRepo to Sales Engine...

--- lib/MetaRepository.rb
+++ lib/MetaRepository.rb
@@ -1,40 +1,49 @@
-require_relative 'sales_engine'
-
-class MetaRepository
-
-  def inspect
-    "#<#{self.class} #{@merchants.size} rows>"
-  end
-
-# this stores all the methods that all the repositories needs
-
-  def all
-    # returns all instances
-  end
-
-  def random
-    #returns a random instance
-  end
-
-  def find_by_atrribute(attribute)
-    #returns a single instance of an attribute which matches the match parameter - case insensisitive
-    # For instance, customer_repository.find_by_first_name("Mary") could find a Customer with the first name attribute "Mary" or "mary" but not "Mary Ellen"
-    # Break apart into all different methods
-    # attributes = customer: id,first_name,last_name,created_at,updated_at
-    # invoice items = id,item_id,invoice_id,quantity,unit_price,created_at,updated_at
-    # invoices = id,customer_id,merchant_id,status,created_at,updated_at
-    # items = id,name,description,unit_price,merchant_id,created_at,updated_at
-    # merchants = id,name,created_at,updated_at
-    # transactions = id,invoice_id,credit_card_number,credit_card_expiration_date,result,created_at,updated_at
-
-    # shared attributes: id,first_name,last_name,created_at,updated_at,item_id,invoice_id,quantity
-    #   unit_price,customer_id,merchant_id,status,name,description,credit_card_number,
-    #   credit_card_expiration_date,result
-  end
-
-  def find_all_by_attribute(attribute)
-    # returns a collection of all matched by attribute. No match returns an empty array
-  end
-
-
-end
+# require_relative 'sales_engine'
+#
+# class MetaRepository
+#
+#
+#   def initialize(repository)
+#     @search_results = []
+#     @repository = repository
+#   end
+#
+#   def inspect
+#     "#<#{self.class} #{@merchants.size} rows>"
+#   end
+#
+# # this stores all the methods that all the repositories needs
+#
+#   def all
+#     @search_results << @repository # returns all instances of the repo
+#   end
+#
+#   def random
+#     @search_results << @repository.sample
+#   end
+#
+#   def find_by_attribute(attribute, criteria)
+#     @search_results << @repository.detect {|x| x[attribute.to_sym] == criteria}
+#
+#     #returns a single instance of an attribute which matches the match parameter - case insensisitive
+#     # For instance, customer_repository.find_by_first_name("Mary") could find a Customer with the first name attribute "Mary" or "mary" but not "Mary Ellen"
+#     # Break apart into all different methods
+#     # attributes = customer: id,first_name,last_name,created_at,updated_at
+#     # invoice items = id,item_id,invoice_id,quantity,unit_price,created_at,updated_at
+#     # invoices = id,customer_id,merchant_id,status,created_at,updated_at
+#     # items = id,name,description,unit_price,merchant_id,created_at,updated_at
+#     # merchants = id,name,created_at,updated_at
+#     # transactions = id,invoice_id,credit_card_number,credit_card_expiration_date,result,created_at,updated_at
+#
+#     # shared attributes: id,first_name,last_name,created_at,updated_at,item_id,invoice_id,quantity
+#     #   unit_price,customer_id,merchant_id,status,name,description,credit_card_number,
+#     #   credit_card_expiration_date,result
+#   end
+#
+#   def find_all_by_attribute(attribute, criteria)
+#     @search_results << @repository.select {|x| x[attribute.to_sym] == criteria}
+#     # returns a collection of all matched by attribute. No match returns an empty array
+#   end
+#
+#
+# end
--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -32,6 +32,8 @@ class SalesEngine
     @item_data         = CSVHandler.open_csv("#{dir}/items.csv")
     @merchant_data     = CSVHandler.open_csv("#{dir}/merchants.csv")
     @transaction_data  = CSVHandler.open_csv("#{dir}/transactions.csv")
+    @search_results = []
+    @repository = repository
   end
 
   def startup
@@ -44,60 +46,95 @@ class SalesEngine
   end
 
   def merchant_relationships
-    merchants_repository.all.each do |merchant|
-      merchant.items    = items_repository.find_all_by_attribute(merchant.id)
-      merchant.invoices = invoices_repository.find_all_by_attribute(merchant.id)
+    @merchants_repository.all.each do |merchant|
+      merchant.items    = @items_repository.find_all_by_attribute(merchant_id, id)
+      merchant.invoices = @invoices_repository.find_all_by_attribute(merchant_id, id)
     end
   end
 
   def customer_relationships
-    customers_repository.all.each do |invoice|
-      customer.invoices = invoices_repository.find_by_attribute(invoice.id)
+    @customers_repository.all.each do |invoice|
+      customer.invoices = @invoices_repository.find_by_attribute(invoice_id, id)
     end
   end
 
   def invoice_item_relationships
-    invoice_items_repository.all.each do |item|
-      invoice_item.item       = items_repository.find_by_attribute(item.id)
+    @invoice_items_repository.all.each do |item|
+      invoice_item.item       = @items_repository.find_by_attribute(item_id, id)
     end
-    invoice_items_repository.all.each do |invoice|
-      invoice_item.invoice    = invoices_repository.find_by_attribute(invoice.id)
+    @invoice_items_repository.all.each do |invoice|
+      invoice_item.invoice    = @invoices_repository.find_by_attribute(invoice_id, id)
     end
   end
 
 
   def invoice_relationships
-    invoices_repository.all.each do |transaction|
-      invoice.transactions    = transactions_repository.find_all_by_attribute(transaction.id)
+    @invoices_repository.all.each do |transaction|
+      invoice.transactions    = @transactions_repository.find_all_by_attribute(transaction_id, id)
     end
-    invoices_repository.all.each do |invoice_item|
-      invoice.invoice_items       = invoice_items_repository.find_all_by_attribute(invoice_item.id)
+    @invoices_repository.all.each do |invoice_item|
+      invoice.invoice_items       = @invoice_items_repository.find_all_by_attribute(invoice_item.id)
     end
-    invoices_repository.all.each do |item|
-      invoice.items           = invoice_items_repository.find_all_by_attribute(item.id)
+    @invoices_repository.all.each do |item|
+      invoice.items           = @invoice_items_repository.find_all_by_attribute(item_id, id)
     end
-    invoices_repository.all.each do |customer|
-      invoice.customer        = customers_repository.find_by_attribute(customer.id)
+    @invoices_repository.all.each do |customer|
+      invoice.customer        = @customers_repository.find_by_attribute(customer_id, id)
     end
-    invoices_repository.all.each do |merchant|
-      invoice.merchant        = merchants_repository.find_by_attribute(merchant.id)
+    @invoices_repository.all.each do |merchant|
+      invoice.merchant        = @merchants_repository.find_by_attribute(merchant_id, id)
     end
   end
 
   def item_relationships
-    items_repository.all.each do |invoice_items|
-      item.invoice_items    = invoice_items_repository.find_all_by_attribute(invoice_items.id)
+    @items_repository.all.each do |invoice_items|
+      item.invoice_items    = @invoice_items_repository.find_all_by_attribute(invoice_items_id, id)
     end
-    items_repository.all.each do |merchant|
-      item.merchant     = merchants_repository.find_by_attribute(merchant.id)
+    @items_repository.all.each do |merchant|
+      item.merchant     = @merchants_repository.find_by_attribute(merchant_id, id)
     end
   end
 
   def transactions_relationships
-    transactions_repository.all.each do |invoice|
-      transaction.inovice    = invoices_repository.find_by_attribute(invoice.id)
+    @transactions_repository.all.each do |invoice|
+      transaction.inovice    = @invoices_repository.find_by_attribute(invoice_id, id)
     end
   end
+
+  def all
+    @search_results << @repository # returns all instances of the repo
+  end
+
+  def random
+    @search_results << @repository.sample
+  end
+
+  def find_by_attribute(attribute, criteria)
+    @search_results << @repository.detect {|x| x[attribute.to_sym] == criteria}
+
+    #returns a single instance of an attribute which matches the match parameter - case insensisitive
+    # For instance, customer_repository.find_by_first_name("Mary") could find a Customer with the first name attribute "Mary" or "mary" but not "Mary Ellen"
+    # Break apart into all different methods
+    # attributes = customer: id,first_name,last_name,created_at,updated_at
+    # invoice items = id,item_id,invoice_id,quantity,unit_price,created_at,updated_at
+    # invoices = id,customer_id,merchant_id,status,created_at,updated_at
+    # items = id,name,description,unit_price,merchant_id,created_at,updated_at
+    # merchants = id,name,created_at,updated_at
+    # transactions = id,invoice_id,credit_card_number,credit_card_expiration_date,result,created_at,updated_at
+
+    # shared attributes: id,first_name,last_name,created_at,updated_at,item_id,invoice_id,quantity
+    #   unit_price,customer_id,merchant_id,status,name,description,credit_card_number,
+    #   credit_card_expiration_date,result
+  end
+
+  def find_all_by_attribute(attribute, criteria)
+    @search_results << @repository.select {|x| x[attribute.to_sym] == criteria}
+    # returns a collection of all matched by attribute. No match returns an empty array
+  end
+
+
+end
+
 
 end
 
